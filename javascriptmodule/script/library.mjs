332c137fd4aad616f5c5904056f1ceaea06b44cb
nouval0425 <ghifarynopal@gmail.com>
2026-04-28 10:03:21 +0700

    moduel js

---
+++ javascriptmodule/script/library.mjs
@@ -0,0 +1,9 @@
+export function saywallahi(name){   //tambah export
+    console.info(`say walllahi ${name}`)
+}
+
+export function saygoodbye(name){ //tambah export
+    console.info(`goodbye ${name}`)
+}
+
+const name = "agie";
